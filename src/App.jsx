a5d2ae785f103f62e7621f993c62568d0580b0c5
Flames <vibe-coder@flames.com>
2025-11-04 02:52:52 +0000

    Update project prj_tmxko0gv - 2025-11-04T02:52:51.930Z

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import TipsAndTricks from './components/TipsAndTricks';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white text-gray-900">
-      <HeroSection />
-      <CategoriesGrid />
-      <AffiliateProducts />
-      <TipsAndTricks />
-      <div className="h-8" />
+    <div className="min-h-screen w-full bg-gray-50 text-gray-900">
+      <div className="mx-auto w-full max-w-[360px] bg-white">
+        <HeroSection />
+        <CategoriesGrid />
+        <AffiliateProducts />
+        <TipsAndTricks />
+        <div className="h-8" />
+      </div>
     </div>
   );
 }
